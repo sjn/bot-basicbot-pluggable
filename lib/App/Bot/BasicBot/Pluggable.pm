b81e0f217fd924bd01f3935d11e9065f2ab29203
Mario Domgoergen <dom@math.uni-bonn.de>
2009-08-11 23:18:40 +0200

    reading module settings from configfile
    This is currently not working due a bug in MooseX::Getopt, which
    does not handle the configgile paramter when it defaults to a
    subroutine ... i have to look into that.

--- lib/App/Bot/BasicBot/Pluggable.pm
+++ lib/App/Bot/BasicBot/Pluggable.pm
@@ -4,27 +4,31 @@ use Moose;
 with 'MooseX::Getopt';
 with 'MooseX::SimpleConfig';
 
-has server  => ( is => 'ro', isa => 'Str', required => 1 );
-has nick    => ( is => 'ro', isa => 'Str', default  => 'basicbot' );
-has charset => ( is => 'ro', isa => 'Str', default  => 'utf8' );
-has channel => ( is => 'ro', isa => 'ArrayRef' );
+has server   => ( is => 'ro', isa => 'Str', required => 1 );
+has nick     => ( is => 'ro', isa => 'Str', default  => 'basicbot' );
+has charset  => ( is => 'ro', isa => 'Str', default  => 'utf8' );
+has channel  => ( is => 'ro', isa => 'ArrayRef' );
 has password => ( is => 'ro', isa => 'Str' );
-has port => ( is => 'ro', isa => 'Int', default => 6667 );
+has port     => ( is => 'ro', isa => 'Int', default => 6667 );
+
+has settings => ( metaclass => 'NoGetopt', is => 'rw', isa => 'HashRef');
 
 has configfile => ( 
     is => 'ro', 
     isa => 'Str', 
+    #builder   => '_build_configfile',
+    default => sub { return Config::Find->find(name => 'bot-basicbot-pluggable.yaml'); },
 );
+
+sub _build_configfile {
+	return Config::Find->find(name => 'bot-basicbot-pluggable.yaml');
+}
 
 has module => (
     is      => 'ro',
     isa     => 'ArrayRef',
     default => sub { return [qw( Auth Loader )] }
 );
-
-sub _build_configfile {
-	return Config::Any->find();
-}
 
 sub BUILDER {
 	my ($self) = @_;
