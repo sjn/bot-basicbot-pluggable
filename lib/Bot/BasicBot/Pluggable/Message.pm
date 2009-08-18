6e3c6b3b6339929b15a75d6f36c61993684ac358
Mario Domgoergen <dom@math.uni-bonn.de>
2009-08-18 18:15:11 +0200

    new attributes command and args

--- lib/Bot/BasicBot/Pluggable/Message.pm
+++ lib/Bot/BasicBot/Pluggable/Message.pm
@@ -5,8 +5,21 @@ has who     => ( is => 'rw', isa => 'Str' );
 has channel => ( is => 'rw', isa => 'Str' );
 has body    => ( is => 'rw', isa => 'Str' );
 has address => ( is => 'rw', isa => 'Str' );
+has prefix  => ( is => 'rw', isa => 'Str', default => '!' );
+
+has command =>
+  ( is => 'rw', isa => 'Str', lazy => 1, builder => '_build_command' );
+has args =>
+  ( is => 'rw', isa => 'Str', lazy => 1, builder => '_build_command' );
 
 __PACKAGE__->meta->make_immutable;
+
+sub _build_command {
+	my ($self) = @_;
+	my ($command,$args) = $self->split();
+	$self->command($command);
+	$self->args([@args]);
+}
 
 sub is_privmsg {
     my ($self) = @_;
